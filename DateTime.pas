6a92b577837313d81afce32638a79905a1c1b0d5
Vladimir Baikov <vbajkov624@gmail.com>
2024-10-04 22:25:10 +0300

    Add sum days between year1 and year2

--- DateTime.pas
+++ DateTime.pas
@@ -5,6 +5,7 @@
   if ((a mod 4 = 0) and (a mod 100 <> 0)) or (a mod 400 = 0) then
     flag := True;
   Print($'Год високосный: {flag}');
+ 
   
   
   var (day1, mounth1) := ReadInteger2('Введите первую дату: (День, месяц)');
@@ -22,10 +23,26 @@
         if day1 <> day2 then
           Print('Дата 2 ближе к новому году')
         else
-          Print('Даты одинаковы')
+          Print('Даты одинаковы');
         
   if flag = True then
     Print('Кол-во дней: 366')
   else
-    Print('Кол-во дней: 365')
+    Print('Кол-во дней: 365');
+  
+  var (year1, year2) := ReadInteger2();
+  var sum := 0;
+  Assert(year1 < year2);
+  Assert((year1 > 0) and (year2 > 0));
+  for var i := year1 to year2 do
+    begin
+    flag:=False;
+    if ((a mod 4 = 0) and (a mod 100 <> 0)) or (a mod 400 = 0) then
+      flag := True;
+    if flag = True then
+      sum+=366
+    else
+      sum+=365;
+    end;
+  Print(sum);
 end.
